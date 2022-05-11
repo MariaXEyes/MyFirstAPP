9dbc9362d3eb9bc2143ed3f281cea0dfd769876d
MariaXEyes <mariaaleinien@gmail.com>
2022-05-11 14:43:07 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int p = 15;
+
+  void _pluscounter() {
+    setState(() {
+      p++;
+    });
+  }
+
+  void _minuscounter() {
+    setState(() {
+      p--;
+    });
+  }
+
+  void _multipleoftwo() {
+    setState(() {
+      p = p * 2;
+    });
+  }
+
+  void reset() {
+    setState(() {
+      p = 15;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Maro First Calc'),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('$p'),
+              ElevatedButton(
+                  onPressed: () => _pluscounter(), child: Text('+1')),
+              ElevatedButton(
+                  onPressed: () => _minuscounter(), child: Text('-1')),
+              ElevatedButton(
+                  onPressed: () => _multipleoftwo(), child: Text('x2')),
+              ElevatedButton(
+                  onPressed: () => setState(() {
+                        return reset();
+                      }),
+                  child: Text('Reset')),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
